2cd357d9ee790a6c631545ac565aa490968ae7cf
Kokila Meddawitage <kmeddawitage@isa.ae>
2025-03-02 10:04:46 +0530

    feat: Add Kafka Docker Compose setup and producers with partitioning strategies
    - Set up Kafka and Zookeeper using Docker Compose
    - Implement Kafka producers with Sticky Partitioner, Round Robin strategy, and Key-based partitioning
    - Configure producer properties for different partitioning methods

---
+++ Kafka/kafkaBasics/src/test/java/org/kcd/ProducerDemoTest.java
@@ -0,0 +1,21 @@
+package org.kcd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProducerDemoTest {
+    /**
+     * Test {@link ProducerDemoUsingStickyPartitioner#main(String[])}.
+     * <p>
+     * Method under test: {@link ProducerDemoUsingStickyPartitioner#main(String[])}
+     */
+    @Test
+    @DisplayName("Test mai(String[])")
+    void testMai() {
+        // TODO: Diffblue Cover was only able to create a partial test for this method:
+        //   Diffblue AI was unable to find a test
+
+        // Arrange and Act
+        ProducerDemoUsingStickyPartitioner.main(new String[]{"Args"});
+    }
+}
